1fd9a1b954e31a322c5b118cfcddb625974641a1
Hannes <hantu447@student.liu.se>
2020-02-01 00:17:18 +0100

    Added classic library

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,15 @@
 require("player")
 require("inventory")
 require("ghelp")
-
+require("draw")
 
 --https://github.com/rxi/tick
-
+--https://github.com/rxi/classic
 
 -- Load some default values for our rectangle.
 function love.load()
 	tick = require "tick"
+	classic = require "classic"
 	inventory.init()
     imgs = ghelp.preloadimgs()
     player.sprite = imgs.player
@@ -23,4 +24,5 @@ end
 function love.draw()
     love.graphics.draw(imgs.map1)
     love.graphics.draw(player.sprite, 0, 0)
+	draw.inventory()
 end
