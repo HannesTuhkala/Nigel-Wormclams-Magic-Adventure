14ae6266b5c237c4a1b331a752839642143e2303
Hannes <hantu447@student.liu.se>
2020-02-01 00:01:14 +0100

    Added tick library

--- src/main.lua
+++ src/main.lua
@@ -2,16 +2,21 @@ require("player")
 require("inventory")
 require("ghelp")
 
+
+--https://github.com/rxi/tick
+
+
 -- Load some default values for our rectangle.
 function love.load()
     imgs = preloadimgs()
+	tick = require "tick"
 	inventory.init()
     x, y, w, h = 20, 20, 60, 20
 end
 
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-    print(player.attributes.strength)
+	tick.update(dt)
 end
 
 -- Draw a coloured rectangle.
