65c3b011f46f2d8465ef63c405f2dd8ccc09b9a9
Hannes <hantu447@student.liu.se>
2020-02-01 15:26:09 +0100

    Moved dialogs to separate file

---
+++ src/dialog.lua
@@ -0,0 +1,10 @@
+local dialog = {}
+
+local start_text_1 = {"Nigel Wormclam", "This is me. Nigel Wormclam. You might be wondering how I ended up here. To cut a long story short, I am the saviour of the world " ..
+								" and I have to save the world before the unthinkable thing happens."}
+local start_text_2 = {"Nigel Wormclam", "Yes. You guessed it. There will be no more Trocadero in the world."}
+
+dialog[1] = start_text_1
+dialog[2] = start_text_2
+
+return dialog
