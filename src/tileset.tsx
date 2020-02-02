57367e5a200dae981491df46516c219c0a54172a
Robin Sliwa <rorrosliwa@gmail.com>
2020-02-02 20:03:49 +0100

    Remove unused files

--- src/tileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="100" tileheight="100" tilecount="3" columns="3">
- <image source="tileset.png" width="300" height="100"/>
- <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="37" y="12" width="23" height="78"/>
-  </objectgroup>
- </tile>
-</tileset>
