25cd7478ee315ea9e774b87efc3937e682668ff5
Hannes <hantu447@student.liu.se>
2020-02-04 16:49:27 +0100

    Fixed memory leak and added hooks.add and hooks.run

--- src/constants.lua
+++ src/constants.lua
@@ -15,4 +15,7 @@ constants.context_menu.hover_color = {0.1529, 0.5098, 0.6470, 1}
 constants.tabs = {}
 constants.tabs.selected_color = {0.8039, 0.7608, 0, 1}
 
+constants.fonts = {}
+constants.fonts.default = love.graphics.newFont("assets/fonts/Pixel UniCode.ttf", 32)
+
 return constants
--- src/hooks.lua
+++ src/hooks.lua
@@ -1,7 +1,25 @@
 local hooks = {}
+hooks.internal = {}
 
-hooks.onGameStart = function()
-	return
+hooks.add = function(event, id, fn)
+	local ev = hooks.internal[event]
+	
+	if not ev then
+		hooks.internal[event] = {fn}
+	else
+		ev = table.insert(ev, fn)
+		hooks.internal[event] = ev
+	end
+end
+
+hooks.run = function(event, ...)
+	local fn_table = hooks.internal[event]
+	if not fn_table then return end
+	
+	local args = {...}
+	for _, fn in pairs(fn_table) do
+		fn(args)
+	end
 end
 
 return hooks
