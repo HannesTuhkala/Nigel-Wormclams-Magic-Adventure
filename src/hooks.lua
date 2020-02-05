28acbecef11c1057f212b2d868cab7a55f6bb38c
Hannes <hantu447@student.liu.se>
2020-02-05 02:47:42 +0100

    Converted tabs to spaces

--- src/hooks.lua
+++ src/hooks.lua
@@ -2,24 +2,24 @@ local hooks = {}
 hooks.internal = {}
 
 hooks.add = function(event, id, fn)
-	local ev = hooks.internal[event]
-	
-	if not ev then
-		hooks.internal[event] = {fn}
-	else
-		table.insert(ev, fn)
-		hooks.internal[event] = ev
-	end
+    local ev = hooks.internal[event]
+    
+    if not ev then
+        hooks.internal[event] = {fn}
+    else
+        table.insert(ev, fn)
+        hooks.internal[event] = ev
+    end
 end
 
 hooks.run = function(event, ...)
-	local fn_table = hooks.internal[event]
-	if not fn_table then return end
-	
-	local args = {...}
-	for _, fn in pairs(fn_table) do
-		fn(args)
-	end
+    local fn_table = hooks.internal[event]
+    if not fn_table then return end
+    
+    local args = {...}
+    for _, fn in pairs(fn_table) do
+        fn(args)
+    end
 end
 
 return hooks
